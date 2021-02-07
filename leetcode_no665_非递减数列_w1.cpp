7487e6ea2f6e2069c4cc904b4b26b5e9b7db3f2d
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-07 18:19:26 +0800

    Add files via upload

---
+++ leetcode_no665_非递减数列_w1.cpp
@@ -0,0 +1,93 @@
+﻿/* ------------------------------------------------------------------|
+给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+
+我们是这样定义一个非递减数列的： 对于数组中所有的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+
+ 
+
+示例 1:
+
+输入: nums = [4,2,3]
+输出: true
+解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+示例 2:
+
+输入: nums = [4,2,1]
+输出: false
+解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+ 
+
+说明：
+
+1 <= n <= 10 ^ 4
+- 10 ^ 5 <= nums[i] <= 10 ^ 5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/non-decreasing-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：8.1 MB, 在所有 C++ 提交中击败了81.94%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+bool checkPossibility(vector<int>& nums) {
+    int n = nums.size();
+    int cnt = 0;
+    for (int i = 0; i < n - 1; i++) {
+        int x = nums[i];
+        int y = nums[i + 1];
+        if (x > y) {
+            cnt++;
+            if (cnt > 1) {
+                return false;
+            }
+            if (i > 0 && y < nums[i - 1]) {
+                nums[i + 1] = x;
+            }
+        }
+    }
+    return true;
+}
